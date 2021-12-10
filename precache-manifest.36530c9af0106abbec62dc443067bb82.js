bea8d0a6f42373a2d00bdc2e03e01383e9d7db59
Ramiro Andres Bedoya Escobar <iamramiroo@gmail.com>
2021-12-10 12:15:06 -0500

    Updates

---
+++ precache-manifest.36530c9af0106abbec62dc443067bb82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2e3680797c95ad25087",
+    "url": "/static/css/main.f7c91d35.chunk.css"
+  },
+  {
+    "revision": "a2e3680797c95ad25087",
+    "url": "/static/js/main.a2e36807.chunk.js"
+  },
+  {
+    "revision": "1d2d3c2866d1cd3f5f52",
+    "url": "/static/js/1.1d2d3c28.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c390ab6bdd0f100a1b270fb4d4cb4273",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1acb60cb3a6f1dde2d619a537c524935.js"
+  "/precache-manifest.36530c9af0106abbec62dc443067bb82.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
